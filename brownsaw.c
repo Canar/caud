9d454c59a08edff3c99793d6fb5a3369ebc80a9d
Benjamin Cook <root@baryon.it>
2025-03-10 08:10:50 -0700

    init

---
+++ brownsaw.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#define FREQ 2048
+#define LEN 16384
+void main(){
+	uint16_t buf[FREQ];
+	uint64_t off=0;
+	uint64_t width,i,flw;
+	srand(time(NULL));
+	
+	while( off < LEN ){
+		flw=(off * FREQ) / LEN;
+		width=( random()%( flw + 1) ) + 2 ;
+		fprintf(stderr,"%d %d %d\n",width, off, flw);
+		for(i=0;i<width;i++){
+			buf[i]=i * ( UINT16_MAX * (LEN - off) / LEN ) / ( width - 1 ) ;
+		}
+		off+=write(1,buf,sizeof(buf[0])*width);
+	}
+}
